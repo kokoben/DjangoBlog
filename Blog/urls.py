315a9b63d44779b1814e3b78c4e075fcc3d263b3
Ben Lee <benzuvsteel@gmail.com>
2017-02-28 14:11:44 -0800

    deleted root forms.py containing LoginForm. deleted authentication_form argument in root login url.

--- Blog/urls.py
+++ Blog/urls.py
@@ -1,14 +1,13 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
-from forms import LoginForm
 
 urlpatterns = [
 	url(r'^', include('home.urls')),
 	url(r'^signup/', include('signup.urls')),
 	url(r'^dash/', include('dashboard.urls')),
 	url(r'^posts/', include('posts.urls')),
-	url(r'^login/$', auth_views.login, { 'authentication_form': LoginForm}, name='login'),
+	url(r'^login/$', auth_views.login, name='login'),
 	url(r'^logout/$', auth_views.logout, {'next_page':'login'}, name='logout'),
     url(r'^admin/', admin.site.urls),
 ]
--- forms.py
+++
@@ -1,6 +0,0 @@
-from django import forms
-from django.contrib.auth.forms import AuthenticationForm
-
-class LoginForm(AuthenticationForm):
-	username = forms.CharField(min_length=1, max_length=150)
-	password = forms.CharField(widget=forms.PasswordInput, min_length=1, max_length=100)
