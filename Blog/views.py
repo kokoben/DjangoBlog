c11a4b861f3e1a0613a8e212a7a15acc46569c53
Ben Lee <benzuvsteel@gmail.com>
2017-03-04 15:44:32 -0800

    removed permanent and query_string attributes from Blog/view.py.

--- Blog/views.py
+++ Blog/views.py
@@ -1,8 +1,6 @@
 from django.views.generic.base import RedirectView
 
 class UserRedirectView(RedirectView):
-	permanent = False
-	query_string = True # what does this do?
 
 	def get_redirect_url(self, username):
 		return '/posts/%s' % username
