155b903563dcff5076e6d295324ca1fa92289ddf
Ben Lee <benzuvsteel@gmail.com>
2017-03-06 13:27:08 -0800

    fixed some code indentation.

--- signup/views.py
+++ signup/views.py
@@ -3,18 +3,18 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import login
 
 def index(request):
-	if request.method == "POST":
+    if request.method == "POST":
 		# create a new user.
-		form = SignupForm(request.POST)
+    	form = SignupForm(request.POST)
 		form_birth = SignupForm2(request.POST)
-		if form.is_valid() and form_birth.is_valid():
-			new_user = form.save(commit=False)	
-			new_user.set_password(form.cleaned_data['password1'])
-			new_user.save()
-			new_user_birth = form_birth.save()
-			new_user.is_active = True
-			login(request, new_user)
-			return redirect('signup:success')
+	if form.is_valid() and form_birth.is_valid():
+		new_user = form.save(commit=False)	
+		new_user.set_password(form.cleaned_data['password1'])
+		new_user.save()
+		new_user_birth = form_birth.save()
+		new_user.is_active = True
+		login(request, new_user)
+		return redirect('signup:success')
 	else:
 		form = SignupForm()
 		form_birth = SignupForm2()
