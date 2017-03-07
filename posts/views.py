f42598e17c1e21d1851245d846a2685cfdf477d1
Ben Lee <benzuvsteel@gmail.com>
2017-03-07 00:08:27 -0800

    working build before changing posts url paths.

--- posts/views.py
+++ posts/views.py
@@ -3,13 +3,13 @@ from django.contrib.auth.models import User
 
 def index(request, username):
     # if username = None, that means client did not input a username after /posts. this means the client wants to see his own posts page, which he can only do if authenticated. So, check if client is logged in as some user. If he is, render posts page with that user. Otherwise, redirect to login page.
-    if username == None:
-        if request.user.is_authenticated():
-            return redirect('index_with_user', username=request.user.username)
-        else:
-            return redirect('login')
-    else:
-        return render(request, 'posts/index.html', {'username': User.objects.get(username=username)})
+	if username == None:
+		if request.user.is_authenticated():
+			return redirect('index_with_user', username=request.user.username)
+		else:
+			return redirect('login')
+	else:
+		return render(request, 'posts/index.html', {'username': User.objects.get(username=username)})
 
 def archive(request):
     return render(request, 'posts/archive.html')
