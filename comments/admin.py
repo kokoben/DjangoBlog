a736e335bb6023f57805457e5432e399ae12bc6f
Ben Lee <benzuvsteel@gmail.com>
2017-03-14 21:53:48 -0700

    added comments to admin site.

--- comments/admin.py
+++ comments/admin.py
@@ -1,10 +1,7 @@
 from django.contrib import admin
-from django.forms import ModelForm
 from .models import Comment
 
-class ArticleForm(ModelForm):
-	class Meta:
-		model = Comment
-		fields = ['body']
+admin.site.register(Comment)
+
 
 
