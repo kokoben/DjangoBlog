7df9fac76ce62010d1f2c593a00e06d12bf213ee
Ben Lee <benzuvsteel@gmail.com>
2017-03-23 00:04:10 -0700

    removed old RedirectView. added logic to redirect authenticated user from log in page to dash.

--- Blog/urls.py
+++ Blog/urls.py
@@ -1,12 +1,13 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from . import views
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
 	url(r'^', include('home.urls')),
 	url(r'^signup/', include('signup.urls')),
 	url(r'^dash/', include('dashboard.urls')),
-	url(r'^login/$', auth_views.login, name='login'),
+	url(r'^login/$', views.custom_login, name='login'),
 	url(r'^logout/$', auth_views.logout, {'next_page':'login'}, name='logout'),
     url(r'^admin/', admin.site.urls),
 	url(r'^(?P<username>\w+)/', include('posts.urls'))
--- Blog/views.py
+++ Blog/views.py
@@ -1,6 +1,10 @@
 from django.views.generic.base import RedirectView
+from django.contrib.auth.views import login
+from django.shortcuts import redirect
 
-class UserRedirectView(RedirectView):
-
-	def get_redirect_url(self, username):
-		return '/posts/%s' % username
+def custom_login(request):
+	""" wrapper of django's login view that redirects user to dash if user is already logged in and tries to access the login page via its url."""
+	if request.user.is_authenticated:
+		return redirect('dashboard:index')
+	else:
+		return login(request)
