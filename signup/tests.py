d6b593d0200acc4e8d270094b6c4f083d88f6a4a
Ben Lee <benzuvsteel@gmail.com>
2017-03-22 23:39:10 -0700

    wrote a signup test case.

--- signup/tests.py
+++ signup/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import SignupInfo
+import datetime
 
-# Create your tests here.
+class SignupTestCase(TestCase):
+	def test_one_to_one(self):
+		"""verify that SignupInfo is correctly related to a user object via a one-to-one relationship."""
+		user = User.objects.create(username="coolperson")
+		# chosen date is arbitrary.
+		additional_info = SignupInfo.objects.create(user=user, birthdate=datetime.date(month=9, day=25, year=1999))
+		user.signupinfo = additional_info
+		user.save()
+		self.assertEqual(user.signupinfo.user, user)
+		self.assertEqual(user.signupinfo.birthdate, datetime.date(month=9, day=25, year=1999))
+
+	
