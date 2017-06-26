6985f54592fc1db78beae4f10f5812387112b354
Ben <benzuvsteel@gmail.com>
2017-06-26 00:22:45 -0700

    revert to f83d4dbde.

--- comments/tests.py
+++ comments/tests.py
@@ -1,27 +1,7 @@
 from django.test import TestCase
-from .models import Comment, Like 
+from .models import Like
 from posts.models import Post
 from django.contrib.auth.models import User
-
-class CommentTestCase(TestCase):
-    def setUp(self):
-        self.user = User.objects.create(username="test_user")
-        self.post = Post.objects.create(title='Test Post', body='test content', user=self.user)
-        self.comment = Comment.objects.create(body="test comment", post=self.post, user=self.user)
-
-    def test_post_has_comment(self):
-        self.assertIn(self.comment, self.post.comment_set.all()) 
-    
-    def test_post_has_comments(self):
-        comment2 = Comment.objects.create(body="test comment 2", post=self.post, user=self.user)
-        commentsList = [self.comment, comment2]
-        allCommentsInList = True
-
-        for comment in commentsList:
-            if comment not in self.post.comment_set.all():
-                allCommentsInList = False
-        
-        self.assertTrue(allCommentsInList)
 
 class LikeTestCase(TestCase):
     def setUp(self):
@@ -45,6 +25,5 @@ class LikeTestCase(TestCase):
                 allLikesInList = False
 
         self.assertTrue(allLikesInList)
-
     
 
