0c91e6608df68666c646b31dc355c52144fc823f
ben <benzuvsteel@gmail.com>
2019-10-16 21:23:47 -0700

    fix login and logout

--- Blog/urls.py
+++ Blog/urls.py
@@ -1,14 +1,14 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 from . import views
-from django.contrib.auth import views as auth_views
+from django.contrib.auth import logout
 
 urlpatterns = [
 	url(r'^', include('home.urls')),
 	url(r'^signup/', include('signup.urls')),
 	url(r'^dash/', include('dashboard.urls')),
 	url(r'^login/$', views.custom_login, name='login'),
-	url(r'^logout/$', auth_views.logout, {'next_page':'login'}, name='logout'),
+	url(r'^logout/$', logout, {'next_page':'login'}, name='logout'),
         url(r'^admin/', admin.site.urls),
 	url(r'^(?P<username>\w+)/', include('posts.urls')),
         url(r'^comments/', include('comments.urls'))
--- Blog/views.py
+++ Blog/views.py
@@ -1,5 +1,5 @@
 from django.views.generic.base import RedirectView
-from django.contrib.auth.views import login
+from django.contrib.auth import login
 from django.shortcuts import redirect
 
 def custom_login(request):
